6fb10e24aeb84ceb11794f35d6b6c3c7e0c6d92e
OzzyFin <ozzfi1@gmail.com>
2017-12-05 20:37:55 +0200

    new-setup

---
+++ src/server/setup.lua
@@ -0,0 +1,19 @@
+--	// Moves everything into their corresponding directories
+
+local baseFolder = script.Parent.Parent.Parent
+local stdModule = baseFolder.modules.SERVERLIST_STD
+stdModule.Parent = game:GetService("ReplicatedStorage")	--	// The only library that needs to be accessed as soon as possible
+local std = require(stdModule)
+
+local serverScripts = baseFolder.src.server
+local clientScripts = baseFolder.src.client
+
+serverScripts.Name = "SERVERLIST_SERVER"
+clientScripts.Name = "SERVERLIST_CLIENT"
+
+serverScripts.Parent = std.Services.ServerScriptService
+clientScripts.Parent = std.Services.StarterPlayer.StarterPlayerScripts
+
+baseFolder.Parent = std.Services.ReplicatedStorage
+Instance.new("BoolValue",baseFolder).Name = "SETUP_COMPLETE"
+
